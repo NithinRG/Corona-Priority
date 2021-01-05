3c84358ea129264dddc78385a008e7c1531d2985
NithinRG <nithinbiju183@gmail.com>
2021-01-05 18:10:42 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-from flask import Flask, redirect, url_for, render_template, request, jsonify
+from flask import Flask, redirect, url_for, render_template, request, session
 from flask_pymongo import PyMongo
 import pickle
 import numpy as np
 import time
 
 app = Flask(__name__)
+app.secret_key = "abc"
 app.config["MONGO_URI"] = "mongodb+srv://admin:Password1@flask-test.p39lo.mongodb.net/test_database?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 model = pickle.load(open('model.pkl', 'rb'))
 
-global loggedIn
-global formFilled
-global formdata
-loggedIn = False
-formFilled = False
-formdata = {}
 
 user_collection = mongo.db.test_collection
 
 
 @app.route("/")
 def home():
-    if not loggedIn:
+    if not 'loggedIn' in session:
         return redirect(url_for("login"))
-    elif not formFilled:
+    elif not 'formFilled' in session:
         return redirect(url_for("form"))
     else:
+        formdata = session['form']
         formattedName = formdata['fname'] + " " + formdata['lname']
         fname = formdata['fname']
         lname = formdata['lname']
@@ -38,21 +34,20 @@ def home():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    global loggedIn
     if request.method == 'POST':
         credentials = request.form.to_dict()
         if credentials['loginId'] == 'Nithin' and credentials['password'] == "pass":
-            loggedIn = True
-            if formFilled:
+            session['loggedIn'] = True
+            if 'formFilled' in session:
                 return redirect(url_for("home"))
             else:
                 return redirect(url_for("form"))
         else:
             return render_template("login.html", incorrect="Incorrect ID or password")
     else:
-        if not loggedIn:
+        if not 'loggedIn' in session:
             return render_template("login.html")
-        elif not formFilled:
+        elif not 'formFilled' in session:
             return redirect(url_for("form"))
         else:
             return redirect(url_for("home"))
@@ -61,17 +56,15 @@ def login():
 @app.route("/form", methods=['GET', 'POST'])
 def form():
     if request.method == 'GET':
-        if not loggedIn:
+        if not 'loggedIn' in session:
             return redirect(url_for("login"))
-        elif formFilled:
+        elif 'formFilled' in session:
             return redirect(url_for("home"))
     return render_template("form.html")
 
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    global formFilled
-    global formdata
     if request.method == 'POST':
         req = request.form
         formdata = req.to_dict()
@@ -90,15 +83,15 @@ def predict():
         timeNow = time.asctime(time.localtime(time.time()))
         formdata['score'] = output
         formdata['time'] = timeNow
+        session['form'] = formdata.copy()
         user_collection.insert_one(formdata)
-        formFilled = True
+        session['formFilled'] = True
     return redirect(url_for("home"))
 
 
 @app.route("/signout")
 def signout():
-    global loggedIn
-    loggedIn = False
+    session.pop('loggedIn', None)
     return redirect(url_for("login"))
 
 
